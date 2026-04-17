e3968ee7e7965cc878f0a8334729a1a717d29cde
Artem Sokolov <the.jelll.2006@gmail.com>
2026-04-17 13:23:47 +0500

    chore: consolidate gitignore rules and untrack IDE files

--- server/src/app.controller.spec.ts
+++ server/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "AutoCare App"', () => {
+      expect(appController.getHello()).toBe('AutoCare App');
     });
   });
 });
